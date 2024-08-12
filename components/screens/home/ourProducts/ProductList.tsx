fbc7bf9954b3f1253b0a7064179e5dc996cd0df4
Zmsyv <msyvzhrb@gmail.com>
2024-08-13 00:33:17 +0400

    responsive product section added

--- components/screens/home/ourProducts/ProductList.tsx
+++ components/screens/home/ourProducts/ProductList.tsx
@@ -8,13 +8,13 @@ const productData = [{}];
 const ProductList = () => {
   return (
     <>
-      <div className="grid grid-cols-4 gap-[32px] ">
+      <div className="">
         <ProductListItem />
       </div>
       <div className=" pt-[20px]  flex justify-center items-center text-center">
         <Button
           variant="outline"
-          className=" w-[245px] text-orange border-orange "
+          className=" w-[245px] sm:w-10 md:w-24 lg:w-40 xl:w-60 text-orange border-orange "
         >
           <p>Show More</p>
         </Button>
--- components/screens/home/ourProducts/ProductListItem.tsx
+++ components/screens/home/ourProducts/ProductListItem.tsx
@@ -5,59 +5,123 @@ import React from "react";
 const productData = [
   {
     id: 1,
-    src: "./images/lamb.svg",
+    src: "./images/chair.svg",
     alt: "",
-    title: "Grifo",
-    category: "Night Lamb",
-    price: "1500 RP",
+    title: "Syltherine",
+    category: "Stylish cafe chair",
+    total:3500000,
     tags: {
-      isNew: true,
+      isNew: false,
       isDiscount: true,
-      discount: "-50",
+      discount: 30,
     },
   },
   {
     id: 2,
-    src: "./images/lamb.svg",
+    src: "./images/chair.svg",
     alt: "",
-    title: "Grifo",
-    category: "Night Lamb",
-    price: "1500 RP",
+    title: "Leviosa",
+    category: "Stylish cafe chair",
+    total: 1500000,
     tags: {
       isNew: false,
-      isDiscount: true,
-      discount: "-50",
+      isDiscount: false,
+      discount: 0,
     },
   },
   {
     id: 3,
-    src: "./images/lamb.svg",
+    src: "./images/sofa.svg",
     alt: "",
-    title: "Grifo",
-    category: "Night Lamb",
-    price: "1500 RP",
+    title: "Lolito",
+    category: "Luxury big sofa",
+    total: 7000000,
+    tags: {
+      isNew: false,
+      isDiscount: true,
+      discount: 50,
+    },
+  },
+  {
+    id: 4,
+    src: "./images/table.svg",
+    alt: "",
+    title: "Respira",
+    category: "Outdoor bar table and stool",
+    total: 500000,
     tags: {
       isNew: true,
       isDiscount: false,
-      discount: "0",
+      discount: 0,
+    },
+  },
+  {
+    id: 5,
+    src: "./images/chair.svg",
+    alt: "",
+    title: "Grifo",
+    category: "dgjwdjhd",
+    total: 1500000,
+    tags: {
+      isNew: false,
+      isDiscount: false,
+      discount: 0,
+    },
+  },
+  {
+    id: 6,
+    src: "./images/mug.svg",
+    alt: "",
+    title: "Muggo",
+    category: "Small mug",
+    total: 150000,
+    tags: {
+      isNew: true,
+      isDiscount: false,
+      discount: 0,
+    },
+  },
+  {
+    id: 7,
+    src: "./images/bed.svg",
+    alt: "",
+    title: "Pinkgy",
+    category: "Cute bed set",
+    total: 7000000 ,
+    tags: {
+      isNew: false,
+      isDiscount: true,
+      discount: 50,
+    },
+  },
+  {
+    id: 8,
+    src: "./images/pot.svg",
+    alt: "",
+    title: "Potty",
+    category: "Minimalist flower pot",
+    total: 500000,
+    tags: {
+      isNew: true,
+      isDiscount: true,
+      discount: 50,
     },
   },
 ];
 
 const ProductListItem = () => {
   return (
-    <div>
-      <ul className=" flex gap-[20px] ">
+    <div className="">
+      <ul className=" grid grid-cols-4 gap-[32px] max-sm:grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
         {productData.map((item) => (
-          <li className=" " key={item.id}>
-            <div className=" relative">
+          <li  className=" w-[285px] h-[446px]" key={item.id}>
+            <div className=" relative ">
               <div className="flex  items-center p-4 gap-2 absolute top-3 right-3 z-10 ">
                 {item.tags.isDiscount && (
                   <div className="bg-red rounded-full w-11 h-11 flex items-center justify-center ">
-                    {item.tags.discount}%
+                    -{item.tags.discount}%
                   </div>
                 )}
-
                 {item.tags.isNew && (
                   <div className="bg-blue rounded-full w-11 h-11 flex items-center justify-center   ">
                     New
@@ -72,7 +136,15 @@ const ProductListItem = () => {
             <div className="pl-[16px] w-[285px] bg-lightOrange1">
               <h3 className=" font-bold text-[24px] pt-4 ">{item.title}</h3>
               <h4 className="text-light py-[8px]">{item.category}</h4>
-              <h3 className="font-bold pb-[30px]">{item.price}</h3>
+              <div className="flex justify-between">
+              <h3 className="font-bold pb-[30px]">{item.total- item.total * item.tags.discount /100} $</h3>
+              {item.tags.isDiscount && (
+                  <div className=" text-light pr-[16px] line-through">
+                    { item.total } $
+                  </div>
+                )}
+
+              </div>
             </div>
           </li>
         ))}

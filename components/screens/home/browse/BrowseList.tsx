1695e68654468209ef7105ae920c2e49e0254d4d
Zmsyv <msyvzhrb@gmail.com>
2024-08-14 19:22:45 +0400

    Link added browse section

--- components/screens/home/browse/BrowseList.tsx
+++ components/screens/home/browse/BrowseList.tsx
@@ -26,10 +26,12 @@ const BrowseList = () => {
       <ul className=" grid grid-cols-3 sm:grid-cols-1 max-sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3  gap-[20px]  ">
         {browseData.map((item) => (
           <li key={item.id}>
+            <Link href="">
             <Image src={item.src} width={381} height={480} alt="" />
             <p className="text-[24px] text-center pt-[30px] font-bold">
               {item.title}
             </p>
+            </Link>
           </li>
         ))}
       </ul>

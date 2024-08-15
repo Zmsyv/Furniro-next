aac8575842506eb3c64e8a2d3c72153c944d978b
Zmsyv <msyvzhrb@gmail.com>
2024-08-16 00:47:04 +0400

    single product page added

---
+++ app/single-product/page.tsx
@@ -0,0 +1,12 @@
+import SingleProductMain from '@/components/screens/product/SingleProductMain'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <SingleProductMain/>
+    </div>
+  )
+}
+
+export default page
